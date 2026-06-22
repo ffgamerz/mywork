692575177db2972fe49d83d93076ad1fbacf8f63
Azman <ffgamerz@gmail.com>
2026-06-23 00:45:53 +0800

    update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
-// src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+// Letakkan URL dan ANON_KEY projek baharu anda terus di sini
+const supabaseUrl = 'https://wvpqllnpataysjqufumy.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2cHFsbG5wYXRheXNqcXVmdW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxMjYzNjksImV4cCI6MjA5NzcwMjM2OX0.TSDZ7SXjcTD6IzUGDVGWEDjcc0rrXeCEC_P-23DCXsk'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
